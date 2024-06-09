5877a97035edf0ac40d43d0a711eda505eec544f
JorgeSayago <sayagojorge2003@gmail.com>
2024-06-09 00:42:30 -0500

    Footer
    creacion del pie de pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceraComponent } from './cabecera/cabecera.component';
+import { PiePaginaComponent } from './pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabeceraComponent
+    CabeceraComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
