479b131ea5d94239c2bc19ad5f564ddf211d79c7
JorgeSayago <sayagojorge2003@gmail.com>
2024-06-17 15:09:10 -0500

    Menu
    funcionalidad del menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductoComponent } from './Page/producto/producto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:"pagina/CrearProducto",component:ProductoComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
