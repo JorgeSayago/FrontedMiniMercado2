d03db9976849ece725ef467eea64f63e7f72acfd
JuanQuizhpi <juanfranciscoqf@gmail.com>
2024-06-18 18:43:30 -0500

    Ruta para login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductoComponent } from './Page/producto/producto.component';
 import { CrearClienteComponent } from './Page/crear-cliente/crear-cliente.component';
 import { TicketComponent } from './Page/ticket/ticket.component';
+import { LoginComponent } from './Page/login/login.component';
 
 const routes: Routes = [
   { path:"pagina/CrearProducto",component:ProductoComponent},
   { path:"pagina/CrearCliente",component:CrearClienteComponent},
   { path:"pagina/crearTicket",component:TicketComponent},
+  { path:"pagina/loginUsuario",component:LoginComponent}
 ];
 
 @NgModule({
