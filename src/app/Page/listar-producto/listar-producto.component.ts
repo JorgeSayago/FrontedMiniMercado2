6ec5a775c4c794d2bc9ed354d2dd9e144b99622b
JuanQuizhpi <juanfranciscoqf@gmail.com>
2024-07-16 11:52:17 -0500

    Funcionalidad Componente Producto
    Crear y listar para el componente Producto

--- src/app/Page/listar-producto/listar-producto.component.ts
+++ src/app/Page/listar-producto/listar-producto.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+import { Producto } from '../../Domain/producto';
+import { ProductoService } from '../../services/producto.service';
 
 @Component({
   selector: 'app-listar-producto',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrl: './listar-producto.component.css'
 })
 export class ListarProductoComponent {
+
+  Producto: Producto = new Producto();
+  listadoProductosWS:any;
+
+  constructor(private productoService: ProductoService,
+    private router: Router) {
+  let params = this.router.getCurrentNavigation()?.extras.queryParams;
+    if(params){
+      this.Producto = new Producto();
+      this.Producto = params['Producto']
+    }
+}
+
+ngOnInit(): void {
+  this.listadoProductosWS = this.productoService.getAll();
+ }
 
 }
